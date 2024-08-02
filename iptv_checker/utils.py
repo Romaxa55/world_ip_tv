df5a3219cf0ae02871f3bbe406d184ff6267f5d2
Roman SHamagin <roman.shamagin@7vpn.com>
2024-08-03 00:07:54 +0300

    feat": add index.m3u

--- iptv_checker/utils.py
+++ iptv_checker/utils.py
@@ -1,9 +1,12 @@
 import asyncio
 import os
+import re
+
 import aiohttp
 import logging
 
 logger = logging.getLogger(__name__)
+
 
 def read_playlist_urls(filepath):
     playlist_urls = {}
@@ -20,6 +23,7 @@ def read_playlist_urls(filepath):
             playlist_urls[country.strip()] = url
     return playlist_urls
 
+
 async def download_playlist(url):
     async with aiohttp.ClientSession() as session:
         try:
@@ -35,10 +39,14 @@ async def download_playlist(url):
 
     return ""
 
+
 def create_index_m3u():
     index_content = "#EXTM3U\n"
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     output_dir = os.path.join(base_dir, 'output')
+
+    # Словарь для хранения каналов по группам
+    grouped_channels = {}
 
     for root, _, files in os.walk(output_dir):
         for file in files:
@@ -47,7 +55,17 @@ def create_index_m3u():
                 with open(file_path, 'r') as f:
                     content = f.read()
                     lines = content.splitlines()[1:]
-                    index_content += "\n".join(lines) + "\n"
+                    for i in range(0, len(lines), 2):
+                        group_title = re.search(r'group-title="([^"]+)"', lines[i]).group(1)
+                        channel_info = f'{lines[i]}\n{lines[i + 1]}\n'
+                        if group_title not in grouped_channels:
+                            grouped_channels[group_title] = []
+                        grouped_channels[group_title].append(channel_info)
+
+    # Сортируем группы по алфавиту
+    for group_title in sorted(grouped_channels):
+        for channel_info in grouped_channels[group_title]:
+            index_content += channel_info
 
     index_path = os.path.join(output_dir, 'index.m3u')
     with open(index_path, 'w') as f:
